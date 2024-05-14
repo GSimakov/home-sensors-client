c78c1af3bed92c56c02bed6a4b64dcb1faad5206
Gosha Simakov <yolo73104@gmail.com>
2024-05-14 14:48:54 +0500

    table update

--- src/components/Measurements/Tables/LightTableByHardwareId.js
+++ src/components/Measurements/Tables/LightTableByHardwareId.js
@@ -1,13 +1,27 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
+import TableFooter from '@mui/material/TableFooter';
+import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { Button } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import LastPageIcon from '@mui/icons-material/LastPage';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import TableHead from '@mui/material/TableHead';
+import { Unstable_NumberInput as NumberInput } from '@mui/base/Unstable_NumberInput';
+
+
+
 
 
 import axios from 'axios';
@@ -20,12 +34,11 @@ import './Table.css'
 export default function LightTableListByHardwareId() {
 
   const [page, setPage] = useState(1);
-  const [size, setSize] = useState(50);
-  
+  const [size, setSize] = useState(20);
+  const [totalSize, setTotalSize] = useState(0);
+  const [showPagination, setShowPagination] = useState(false)
   const [hardwareId, setHardwareId] = useState('');
-
   const [measurementsList, setMeasurementsList] = useState([]);
-
   const urlList = 'http://192.168.0.10:8001/api/data/light/list_hid'
 
   const fetchDataList = async (url) => {
@@ -37,34 +50,57 @@ export default function LightTableListByHardwareId() {
       },
     })
 
-    const items = response.data.data.items;
+    console.log(response);
 
-    for (let index = 0; index < items.length; index++) {
+    const items = response.data.data.items;
+    const total = response.data.data.total;
+
+
+    for (let index = 0; index < items - 1; index++) {
       const element = items[index];
       const correctDatetime = new Date(element.created_at);
       element.created_at = correctDatetime.toString();
     }
     
+
+    setTotalSize(total);
     setMeasurementsList(items);
 
+    if (total !== 0) {
+        setShowPagination(true);
+    } else {
+        setShowPagination(false);
+      }
+      
   }
-  
-  useEffect(() => {
-    fetchDataList(urlList);
-  }, []);
-  
+
+
 
   const handleChangeHardwareId = (event) => {
     setHardwareId(event.target.value);
   };
 
-    const handleChangePage = (event) => {
-        setPage(event.target.value);
-    };
+  const handleChangePage = (event, newPage) => {
 
-    const handleChangeSize = (event) => {
-        setSize(event.target.value);
-    };
+    if (newPage <= 0){
+        setPage(1);
+    } else {
+        setPage(newPage);
+    }
+
+    fetchDataList(urlList);
+    
+  };
+
+
+
+
+    function defaultLabelDisplayedRows({ from, to, count })
+    { 
+        return  `${from - size}–${to - size} of ${count !== -1 ? count : `more than ${to}`}`; 
+    }
+
+
  
   return (
     <div>
@@ -72,9 +108,7 @@ export default function LightTableListByHardwareId() {
       <h1>List of light measurements by hardware id</h1>
 
       <div className='Params' align="center">
-        <TextField style={{width: "50%", margin: "30px"}} className="Param" id="outlined-basic" label="Hardware ID" variant="outlined" type="text" onChange={handleChangeHardwareId}/>
-        <TextField style={{width: "15%", margin: "30px"}} className="Param" label="Page" variant="standard" type="number" onChange={handleChangePage}/>
-        <TextField style={{width: "15%", margin: "30px"}} className="Param" label="Size" variant="standard" type="number" onChange={handleChangeSize}/>
+        
       </div>
           
     <div className='Table'>
@@ -103,6 +137,54 @@ export default function LightTableListByHardwareId() {
                     </TableRow>
                     ))}
                 </TableBody>
+
+
+
+
+                <TableFooter>
+                    <TableRow style={{width: "100%", margin: "0px"}}>
+
+                        <TextField 
+                        style={{width: "100%", margin: "5px"}} 
+                        id="outlined-basic" 
+                        label="Hardware ID" 
+                        variant="outlined" 
+                        type="text" 
+                        onChange={handleChangeHardwareId}/>
+
+                        {showPagination ?
+                            <TablePagination
+                            style={{width: "20%", margin: "0px", padding: '0px'}}
+                            colSpan={4}
+                            rowsPerPageOptions={[]}
+                            count={totalSize}
+                            rowsPerPage={size}
+                            page={page}
+                            slotProps={{
+                                select: {
+                                inputProps: {
+                                    'aria-label': 'rows per page',
+                                },
+                                native: true,
+                                },
+                            }}
+                            onPageChange={handleChangePage}
+                            ActionsComponent={TablePaginationActions}
+                            labelDisplayedRows={defaultLabelDisplayedRows}
+                            />
+                        : null }
+                    
+
+                        
+                    </TableRow>
+                </TableFooter>
+
+
+
+
+
+
+
             </Table>
         </TableContainer>
     </div>
@@ -111,3 +193,61 @@ export default function LightTableListByHardwareId() {
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function TablePaginationActions(props) {
+    const theme = useTheme();
+    const { count, page, rowsPerPage, onPageChange } = props;
+  
+  
+    const handleBackButtonClick = (event) => {
+      onPageChange(event, page - 1);
+    };
+  
+    const handleNextButtonClick = (event) => {
+      onPageChange(event, page + 1);
+    };
+  
+  
+    return (
+      <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+
+        <IconButton
+          onClick={handleBackButtonClick}
+          disabled={page === 1}
+          aria-label="previous page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+        </IconButton>
+        <IconButton
+          onClick={handleNextButtonClick}
+          disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+          aria-label="next page"
+        >
+          {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+        </IconButton>
+       
+      </Box>
+    );
+  }
+  
+  TablePaginationActions.propTypes = {
+    count: PropTypes.number.isRequired,
+    onPageChange: PropTypes.func.isRequired,
+    page: PropTypes.number.isRequired,
+    rowsPerPage: PropTypes.number.isRequired,
+    labelDisplayedRows: PropTypes.func.isRequired
+  };
