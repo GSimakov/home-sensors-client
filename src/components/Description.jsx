167daa39576f3a3aec77fb88b787e6ed833c8841
Gosha Simakov <yolo73104@gmail.com>
2024-05-15 20:31:33 +0500

    decoration

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,9 +1,47 @@
 import React from "react";
+import './Description.css'
 
 function Description() {
   return (
     <div className="Description">
-      <p>DESCRIPTION PLACEHOLDER</p>
+      <h1>HOME SENSORS</h1>
+      <h2>graduation project</h2>
+
+      <div className="DescriptionBody">
+        <p>HomeSensors is a service used for managing and administering home
+          data acquisition systems.</p>
+        <p>The service was developed as graduation project</p>
+
+        <div className="GitHub">
+          </div>
+          <h3>GitHub:</h3>
+          <ul>
+            <li><a href="https://github.com/GSimakov/home-sensors-configuration-server">Configuration Server</a></li>
+            <li><a href="https://github.com/GSimakov/home-sensors-measurements-server">Measurements Server</a></li>
+            <li><a href="https://github.com/GSimakov/home-sensors-client">User Interface</a></li>
+            <li><a href="https://github.com/GSimakov/home-sensors-board">Board Software</a></li>
+          </ul>
+      </div>
+      
+
+      <div className="Author">
+        <h3>Author</h3>
+        <p>Simakov Georgiy</p>
+        <p>UISI</p>
+        <p>2024</p>
+
+
+      </div>
+
+
+
+
+        
+
+      
+      
+      
+      
     </div>
   );
 }
