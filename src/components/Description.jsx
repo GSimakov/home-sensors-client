f598bbfbd9871dae1d1d184fc63c36a762faf82a
Gosha Simakov <yolo73104@gmail.com>
2024-05-16 20:11:16 +0500

    decoration

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -13,14 +13,27 @@ function Description() {
         <p>The service was developed as graduation project</p>
 
         <div className="GitHub">
-          </div>
+          
           <h3>GitHub:</h3>
           <ul>
+            <li><a href="https://github.com/GSimakov/home-sensors-build">Build</a></li>
             <li><a href="https://github.com/GSimakov/home-sensors-configuration-server">Configuration Server</a></li>
             <li><a href="https://github.com/GSimakov/home-sensors-measurements-server">Measurements Server</a></li>
             <li><a href="https://github.com/GSimakov/home-sensors-client">User Interface</a></li>
             <li><a href="https://github.com/GSimakov/home-sensors-board">Board Software</a></li>
           </ul>
+        </div>
+
+        <div className="DockerHub">
+          
+          <h3>DockerHub:</h3>
+          <ul>
+            <li><a href="https://hub.docker.com/r/goshasimakov/home-sensors-client/tags">User Interface</a></li>
+            <li><a href="https://hub.docker.com/r/goshasimakov/home-sensors-data/tags">Measurements Server</a></li>
+            <li><a href="https://hub.docker.com/r/goshasimakov/home-sensors-configuration/tags">Configuration Server</a></li>
+          </ul>
+        </div>
+
       </div>
       
 
